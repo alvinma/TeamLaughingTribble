2b370b9420226fdce62e6f73aea2beae5f9f1400
Marco Sandoval <marcosandoval68@gmail.com>
2017-10-24 14:25:33 -0700

    Set up environment and added a login button

--- ParkHere/app/src/main/java/edu/sjsu/thelaughingtribble/parkhere/MainActivity.java
+++ ParkHere/app/src/main/java/edu/sjsu/thelaughingtribble/parkhere/MainActivity.java
@@ -2,6 +2,8 @@ package edu.sjsu.thelaughingtribble.parkhere;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick_logIn(View v){
+        Boolean authentication = authentication(v);
+    }
+
+    private Boolean authentication(View v){
+        String auth_message;
+        if(1 == 1){
+            auth_message = "Logging In";
+        }
+        else{
+            auth_message = "Could not authenticate you";
+        }
+        Toast.makeText(this, auth_message, Toast.LENGTH_SHORT).show();
+        return Boolean.TRUE;
     }
 }
