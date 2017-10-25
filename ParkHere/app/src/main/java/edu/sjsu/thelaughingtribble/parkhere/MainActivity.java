eb0caffb94d757ce94e922a726911f681fd3ff37
alvinma <alvinma111@gmail.com>
2017-10-24 20:19:44 -0700

    Basic ListView for Funtionality Purposes
    Created a simple listview so that we can begin creating the User and owner interface.

--- ParkHere/app/src/main/java/edu/sjsu/thelaughingtribble/parkhere/MainActivity.java
+++ ParkHere/app/src/main/java/edu/sjsu/thelaughingtribble/parkhere/MainActivity.java
@@ -1,7 +1,9 @@
 package edu.sjsu.thelaughingtribble.parkhere;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void parkingList(View v){
+        Intent intent = new Intent(this, ParkingSpotList.class);
+        this.startActivity(intent);
     }
 }
