112f6af9f81171b14b329b257337de7d7a3f372f
alvinma <alvinma111@gmail.com>
2017-10-25 13:26:07 -0700

    Creating Owner's Parking Listing
    Created a new class that will handle the owner's listing of their parking spot.

---
+++ ParkHere/app/src/main/java/edu/sjsu/thelaughingtribble/parkhere/CreateParkingSpotListing.java
@@ -0,0 +1,18 @@
+package edu.sjsu.thelaughingtribble.parkhere;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class CreateParkingSpotListing extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_parking_spot_listing);
+
+
+    }
+
+}
