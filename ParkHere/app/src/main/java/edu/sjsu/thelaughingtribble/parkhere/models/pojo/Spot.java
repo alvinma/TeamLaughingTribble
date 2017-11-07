96752651d47b5062db2de272e84204ada766f07c
alvinma <alvinma111@gmail.com>
2017-11-06 22:06:56 -0800

    Naming Convention was done here
    Forgot to commit. Naming convention change was done here!

--- ParkHere/app/src/main/java/edu/sjsu/thelaughingtribble/parkhere/models/pojo/Spot.java
+++ ParkHere/app/src/main/java/edu/sjsu/thelaughingtribble/parkhere/models/pojo/Spot.java
@@ -71,7 +71,7 @@ public class Spot extends Place{
         this.price = price;
     }
 
-    public boolean isPermitRequired() {
+    public boolean getPermitRequired() {
         return permitRequired;
     }
 
