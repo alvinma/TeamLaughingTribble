7aafc45817edb39177ee23d4894619cc8b74906c
alvinma <alvinma111@gmail.com>
2017-11-09 23:35:57 -0800

    Re-adding google maps experiment

---
+++ ParkHere/app/src/main/java/edu/sjsu/thelaughingtribble/parkhere/controllers/GoogleMaps.java
@@ -0,0 +1,21 @@
+package edu.sjsu.thelaughingtribble.parkhere.controllers;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import edu.sjsu.thelaughingtribble.parkhere.R;
+
+/**
+ * Created by Alvin on 11/9/2017.
+ */
+
+public class GoogleMaps extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_googlemaps);
+
+    }
+}
