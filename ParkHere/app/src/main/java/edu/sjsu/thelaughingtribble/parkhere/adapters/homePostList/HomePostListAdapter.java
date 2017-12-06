df245d58ce7fa8068ee9c9218761e2ca277f7250
alvinma <alvinma111@gmail.com>
2017-12-05 21:07:28 -0800

    Revert "Merge remote-tracking branch 'origin/master' into users/alvin_m/secondary_test_branch"
    This reverts commit 88f714c069a869f236e302c8d229326935c422e0, reversing
    changes made to 2a5eaa805c0aa320624dcad091b153755f62c63a.

--- ParkHere/app/src/main/java/edu/sjsu/thelaughingtribble/parkhere/adapters/homePostList/HomePostListAdapter.java
+++ ParkHere/app/src/main/java/edu/sjsu/thelaughingtribble/parkhere/adapters/homePostList/HomePostListAdapter.java
@@ -1,24 +1,17 @@
 package edu.sjsu.thelaughingtribble.parkhere.adapters.homePostList;
 
-import android.content.Intent;
-import android.support.design.widget.Snackbar;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 
 import edu.sjsu.thelaughingtribble.parkhere.R;
-import edu.sjsu.thelaughingtribble.parkhere.Utils.Constant;
-import edu.sjsu.thelaughingtribble.parkhere.controllers.PostDetailActivity;
-import edu.sjsu.thelaughingtribble.parkhere.models.pojo.Owner;
 import edu.sjsu.thelaughingtribble.parkhere.models.pojo.Post;
-import edu.sjsu.thelaughingtribble.parkhere.models.pojo.Spot;
 import edu.sjsu.thelaughingtribble.parkhere.models.pojo.User;
 
 /**
@@ -38,19 +31,6 @@ public class HomePostListAdapter extends RecyclerView.Adapter<homePostListItemVi
 
     }
 
-    public HomePostListAdapter(ArrayList<Post> posts, User user) {
-        this.posts = posts;
-        this.user = user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
     @Override
     public homePostListItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View mView = LayoutInflater.from(parent.getContext()).inflate(R.layout.home_post_list_item, parent, false);
@@ -60,20 +40,14 @@ public class HomePostListAdapter extends RecyclerView.Adapter<homePostListItemVi
 
     @Override
     public void onBindViewHolder(homePostListItemViewHolder holder, int position) {
-        final Post currentPost = posts.get(position);
-
+        Post currentPost = posts.get(position);
+        Log.i("currentPost title",  currentPost.getTitle());
+        homePostListUI.setUser(user);
+        homePostListUI.setPost(currentPost);
         homePostListUI.getDatePosted().setText(currentPost.getDatePosted());
         homePostListUI.getPostTitle().setText(currentPost.getTitle());
-        if(currentPost.getOwner() == null){
-            currentPost.setOwner(new Owner());
-        }
-        homePostListUI.getPostOwner().setText(currentPost.getOwner().getFullName());
-
-        if(currentPost.getSpot() == null){
-            currentPost.setSpot(new Spot());
-        }
-        homePostListUI.getPostPrice().setText(String.valueOf("$"+currentPost.getSpot().getPrice()));
-        homePostListUI.getPostTitle().setText(String.valueOf(currentPost.getTotalGrade()));
+        homePostListUI.getPostOwner().setText(currentPost.getOwner().getUserID());
+        homePostListUI.getPostPrice().setText(String.valueOf("$ "+currentPost.getSpot().getPrice()));
         homePostListUI.getPostDescription().setText(currentPost.getSpot().getDescription());
         homePostListUI.getTotalRate().setText(String.valueOf(currentPost.getTotalGrade()));
 
@@ -82,17 +56,6 @@ public class HomePostListAdapter extends RecyclerView.Adapter<homePostListItemVi
         } else {
             homePostListUI.getPostImage().setImageResource(R.drawable.not_available);
         }
-
-        homePostListUI.itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), PostDetailActivity.class);
-                intent.putExtra(Constant.DEBUGGING, true);
-                intent.putExtra(Constant.INTENT_EXTRA_POST, currentPost);
-                intent.putExtra(Constant.INTENT_EXTRA_USER, user);
-                view.getContext().startActivity(intent);
-            }
-        });
     }
 
     @Override
